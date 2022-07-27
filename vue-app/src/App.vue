06ccf8aae19f29a7302c9de3e3c664477345037c
theroadfreak <dushancimbalevic@gmail.com>
2022-07-27 15:55:13 +0200

    vue proj succsesful

---
+++ vue-app/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import ChildComp from "./components/ChildComp.vue";
+
+export default {
+  components: {
+    ChildComp,
+  },
+  data() {
+    return {
+      msg: "from parent",
+      view: true,
+    };
+  },
+  methods: {
+    click() {
+      this.view = !this.view;
+    },
+  },
+};
+</script>
+
+<template>
+  <ChildComp />
+
+  <button @click="click()">Press me</button>
+  <h1 v-if="view">Dushan</h1>
+</template>
