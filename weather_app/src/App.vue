fc98328cffe4c25ebcd9e69c051853b2275fdbad
theroadfreak <dushancimbalevic@gmail.com>
2022-07-28 01:02:48 +0200

    deployment build

--- weather_app/src/App.vue
+++ weather_app/src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="appClass" :id="customID">
-    <main>
-      <div class="search-box">
-        <input
-          v-model="query"
-          @keypress="fetchWeather"
-          type="text"
-          class="search-bar"
-          placeholder="Search..."
-        />
-      </div>
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="location-box">
-          <div class="location">
-            {{ weather.name }}, {{ weather.sys.country }} {{ customID }}
+  <div class="container">
+    <div class="appClass" :id="customID">
+      <main>
+        <div class="search-box">
+          <input
+            v-model="query"
+            @keypress="fetchWeather"
+            type="text"
+            class="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+          <div class="location-box">
+            <div class="location">
+              {{ weather.name }}, {{ weather.sys.country }}
+            </div>
+            <div class="date">{{ dateBuilder() }}</div>
           </div>
-          <div class="date">{{ dateBuilder() }}</div>
-        </div>
 
-        <div class="weather-box">
-          <div class="temp">{{ weather.main.temp.toFixed(1) }}°c</div>
-          <div class="weather">{{ weather.weather[0].main }}</div>
+          <div class="weather-box">
+            <div class="temp">{{ weather.main.temp.toFixed(1) }}°c</div>
+            <div class="weather">{{ weather.weather[0].main }}</div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -36,7 +38,7 @@ export default {
       url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
-      customID: "app",
+      customID: "appp",
     };
   },
   methods: {
@@ -57,7 +59,7 @@ export default {
       if (this.weather.main.temp > 20) {
         this.customID = "appHot";
       } else {
-        this.customID = "app";
+        this.customID = "appp";
       }
     },
     dateBuilder() {
@@ -87,109 +89,5 @@ export default {
     },
   },
   //https://api.openweathermap.org/data/2.5/weather?q=London&appid=a46f33595dcc1b2d32ae2645dfe42df2
-  //https://api.openweathermap.org/data/2.5/weather?q=London&appid=a46f33595dcc1b2d32ae2645dfe42df2
 };
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: "montserrat", sans-serif;
-}
-.appClass {
-  background-size: cover;
-  background-position: bottom;
-  transition: 0.4s;
-}
-#appHot {
-  background-image: url("./assets/warm-bg.jpg");
-}
-
-#app {
-  background-image: url("./assets/cold-bg.jpg");
-}
-main {
-  min-height: 100vh;
-  padding: 25px;
-
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0.25),
-    rgba(0, 0, 0, 0.75)
-  );
-}
-
-.search-box {
-  width: 100%;
-  margin-bottom: 30px;
-}
-
-.search-box .search-bar {
-  display: block;
-  width: 100%;
-  padding: 15px;
-
-  font-size: 20px;
-  appearance: none;
-  border: none;
-  outline: none;
-  background: none;
-
-  box-shadow: 0px 0px 8px;
-  background-color: rgba(255, 255, 255, 0.5);
-  transition: 0.4s;
-  border-radius: 0px 16px 0px 16px;
-}
-
-.search-box .search-bar:focus {
-  box-shadow: 0px 0px 16px;
-  background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px 16px 0px;
-}
-
-.location-box .location {
-  color: #fff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-}
-
-.location-box .date {
-  color: #fff;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
-}
-.weather-box {
-  text-align: center;
-}
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
-  font-weight: 900;
-
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 16px;
-  margin: 30px;
-
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-
-.weather-box .weather {
-  color: #fff;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-</style>
